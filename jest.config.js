77ac31934963683fc8c6907500843c6569966ea1
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2021-04-22 10:47:32 -0500

    Adding coverage to tests. Adjusting package.json scripts to run all tests more simply. Making librarires publishable

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   projects: [
     "<rootDir>/apps/react-portal",
     "<rootDir>/apps/angular-portal",
-    "<rootDir>/libs/react-auth0",
     "<rootDir>/libs/angular-auth0",
+    "<rootDir>/libs/react-auth0",
+    "<rootDir>/libs/auth0"
   ],
 };
---
+++ libs/auth0/src/lib/auth0.spec.ts
@@ -0,0 +1,7 @@
+import { auth0 } from './auth0';
+
+describe('auth0', () => {
+    it('should work', () => {
+        expect(auth0()).toEqual('auth0');
+    })
+})
