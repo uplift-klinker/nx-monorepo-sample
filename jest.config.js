9129b86b7dbc02edc11e9478bd548df4e659a15c
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2021-04-22 10:56:53 -0500

    Updating auth0 library to be buildable and publishable

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
     "<rootDir>/apps/react-portal",
     "<rootDir>/apps/angular-portal",
     "<rootDir>/libs/angular-auth0",
-    "<rootDir>/libs/react-auth0",
-    "<rootDir>/libs/auth0"
+    "<rootDir>/libs/react-auth0","<rootDir>/libs/auth0"
   ],
 };
